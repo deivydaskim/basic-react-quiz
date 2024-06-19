3d68e929ac831db95847fc363c7938e763492739
deivydas.kim <deivydas.kim@gmail.com>
2024-06-19 12:20:24 +0300

    add shuffling answers, summary after finishing quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react';
 
+import quizCompleteImg from '../assets/quiz-complete.png';
 import QUESTIONS from '../questions';
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function HandleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswers) => {
@@ -12,17 +22,32 @@ const Quiz = () => {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Quiz completed" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+
+  const shuffeledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffleArray(shuffeledAnswers);
+
   return (
-    <div id="questions">
-      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-      <p>{QUESTIONS[activeQuestionIndex].answers}</p>
-      <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-          <li key={answer} className="answer">
-            <button onClick={() => HandleSelectAnswer(answer)}>{answer}</button>
-          </li>
-        ))}
-      </ul>
+    <div id="quiz">
+      <div id="questions">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {shuffeledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => HandleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
