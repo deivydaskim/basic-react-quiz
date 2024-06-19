7cda3fb70c2a344bf591b6536cf9276bbf5f9bb5
deivydas.kim <deivydas.kim@gmail.com>
2024-06-19 17:00:34 +0300

    create summary with results

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,8 @@
 import { useState, useCallback, act } from 'react';
 
-import quizCompleteImg from '../assets/quiz-complete.png';
 import QUESTIONS from '../questions';
 import Question from './Question';
+import Summary from './Summary';
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
@@ -25,12 +25,7 @@ const Quiz = () => {
   );
 
   if (quizIsComplete) {
-    return (
-      <div id="summary">
-        <img src={quizCompleteImg} alt="Quiz completed" />
-        <h2>Quiz Completed</h2>
-      </div>
-    );
+    return <Summary userAnswers={userAnswers} />;
   }
 
   return (
